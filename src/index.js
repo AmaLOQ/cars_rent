674f084b4c3e4c0196154d18ad342d2ce8960dac
Alexey Muhametshin <muhametshin.alexey@gmail.com>
2022-11-21 17:18:41 +0700

    starter pack

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './fonts/fonst.css'
+import reportWebVitals from './reportWebVitals';
+import App from './app/app';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
+
