fa00a67287748a691f9ba83949e8bdda8ad7e5a0
Alexey Muhametshin <muhametshin.alexey@gmail.com>
2023-03-03 12:08:48 +0700

    Row - name car

---
+++ src/ui/molecules/dotted-text-group/dotted-text-group-styles.js
@@ -0,0 +1,8 @@
+import { Box, styled } from "@mui/system";
+
+export const DottedTextContainer = styled(Box)`
+	margin-top: 18px;
+	gap: 8px;
+	display: flex;
+	flex-wrap: wrap;
+`;
---
+++ src/ui/molecules/dotted-text-group/dotted-text.data.js
@@ -0,0 +1,6 @@
+export const dottedData = [
+	{text: 'Эконом', isUpper: true, isBlack: false},
+	{text: 'Chevrolet', isUpper: true, isBlack: false},
+	{text: 'Aveo', isUpper: true, isBlack: true},
+
+]
